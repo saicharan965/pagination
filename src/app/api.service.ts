eaace9be662a05a23caae1f307ed55f574bc5617
Sai Charan <kutikuppalasaicharan97@gmail.com>
2025-04-17 09:55:51 +0530

    added pagination

---
+++ src/app/api.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Task } from './tasks.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ApiService {
+  #http = inject(HttpClient);
+  constructor() { }
+  getTaks(): Observable<Task[]> {
+    return this.#http.get<Task[]>('https://jsonplaceholder.typicode.com/todos');
+  }
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ApiService } from './api.service';
+import { Task } from './tasks.model';
+import { map } from 'rxjs';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [NgFor],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pagination';
+  protected tasks!: Task[];
+  protected paginatedTasks!: Task[];
+  protected currentPage = 1;
+  protected pageSize = 10;
+  protected totalPages!: number;
+
+  #apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this.#apiService.getTaks().subscribe((tasks) => {
+      this.tasks = tasks;
+    this.totalPages = Math.ceil(this.tasks.length / this.pageSize);
+
+      this.updatePaginatedTasks();
+    });
+  }
+
+  updatePaginatedTasks(): void {
+    const startIndex = (this.currentPage - 1) * this.pageSize;
+    this.paginatedTasks = this.tasks.slice(startIndex, startIndex + this.pageSize);
+  }
+
+  goToPage(page: number): void {
+    this.currentPage = page;
+    this.updatePaginatedTasks();
+  }
 }
+
+
